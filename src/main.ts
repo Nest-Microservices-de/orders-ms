c0f45fc345b247ff3fe3a89b56f892a08d530822
123Bfeo <ldnr13@hotmail.com>
2026-07-18 12:03:42 -0500

    Subiendo codigo de miscroservcio orders-ms

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { RpcCustomExceptionFilter } from './common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+  app.useGlobalFilters(new RpcCustomExceptionFilter());
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
